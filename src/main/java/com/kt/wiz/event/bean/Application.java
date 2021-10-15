d7f7ff3e915934aab808a0d9818dd2b128646654
davidhungpil <davidhungpil@naver.com>
2021-10-15 12:46:33 +0900

    added data field for application table

--- src/main/java/com/kt/wiz/event/bean/Application.java
+++ src/main/java/com/kt/wiz/event/bean/Application.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -42,6 +44,9 @@ public class Application implements Serializable {
 	private String userId;
 	
 	private String choice;
+	
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date regdate = new Date();     
 	
 	@Builder
 	public Application(
